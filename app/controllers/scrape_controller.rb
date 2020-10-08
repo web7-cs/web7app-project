f1cc30f80dca25de72d34ea44514726e49e720eb
panuvit.chantara <panuvit.chantara@ait.asia>
2020-10-09 02:39:24 +0700

    Fix proxy Web Scrapping Panuvit

--- app/controllers/scrape_controller.rb
+++ app/controllers/scrape_controller.rb
@@ -4,7 +4,8 @@ require 'open-uri'
 class ScrapeController < ApplicationController
   
   def index
-    page = Nokogiri::HTML(open("https://www.foxnews.com/world", proxy: URI.parse("http://192.41.170.23:3128"))
+    fetchurl = "https://www.foxnews.com/world"
+    page = Nokogiri::HTML(open(fetchurl, proxy: URI.parse('http://192.41.170.23:3128')))
     @title = page.css("#wrapper > div.page > div.page-content > div > main > div:nth-child(1) > div > article > div.info > header > h2 > a").text
     content_page = page.css("#wrapper > div.page > div.page-content > div > main > div:nth-child(1) > div > article > div.info > header > h2 > a").attr('href')
 
@@ -13,7 +14,7 @@ class ScrapeController < ApplicationController
     else
       content_page = 'https://www.foxnews.com' + '' + content_page
     end
-    content_page_url = Nokogiri::HTML(open(content_page, proxy: URI.parse("http://192.41.170.23:3128"))
+    content_page_url = Nokogiri::HTML(open(content_page, proxy: URI.parse('http://192.41.170.23:3128')))
     @content = content_page_url.css("#wrapper > div.page-content > div.row.full > main > article > div > div.article-content > div.article-body > p").text
     @imageUrl = page.css("#wrapper > div.page > div.page-content > div > main > div:nth-child(1) > div > article > div.m > a > picture > img").attr('src')
   end
