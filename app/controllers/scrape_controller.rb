ed880232ee31b9969292300431525b98e7053fca
panuvit.chantara <panuvit.chantara@ait.asia>
2020-10-09 10:00:25 +0700

    Fetching Fix Panuvit

--- app/controllers/scrape_controller.rb
+++ app/controllers/scrape_controller.rb
@@ -6,8 +6,8 @@ class ScrapeController < ApplicationController
   def index
     fetchurl = "https://www.foxnews.com/world"
     page = Nokogiri::HTML(open(fetchurl, proxy: URI.parse('http://192.41.170.23:3128')))
-    @title = page.css("#wrapper > div.page > div.page-content > div > main > div:nth-child(1) > div > article > div.info > header > h2 > a").text
-    content_page = page.css("#wrapper > div.page > div.page-content > div > main > div:nth-child(1) > div > article > div.info > header > h2 > a").attr('href')
+    @title = page.at_css("#wrapper > div.page > div.page-content > div > main > div:nth-child(1) > div > article > div.info > header > h2 > a").text
+    content_page = page.at_css("#wrapper > div.page > div.page-content > div > main > div:nth-child(1) > div > article > div.info > header > h2 > a").attr('href')
 
     if content_page['https://www.foxnews.com']
       content_page = content_page
@@ -15,8 +15,8 @@ class ScrapeController < ApplicationController
       content_page = 'https://www.foxnews.com' + '' + content_page
     end
     content_page_url = Nokogiri::HTML(open(content_page, proxy: URI.parse('http://192.41.170.23:3128')))
-    @content = content_page_url.css("#wrapper > div.page-content > div.row.full > main > article > div > div.article-content > div.article-body > p").text
-    @imageUrl = page.css("#wrapper > div.page > div.page-content > div > main > div:nth-child(1) > div > article > div.m > a > picture > img").attr('src')
+    @content = content_page_url.at_css("#wrapper > div.page-content > div.row.full > main > article > div > div.article-content > div.article-body > p").text
+    @imageUrl = page.at_css("#wrapper > div.page > div.page-content > div > main > div:nth-child(1) > div > article > div.m > a > picture > img").attr('src')
   end
 
 end
