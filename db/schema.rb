356849a3ac59bbeb8b8cbf01ca24988da397bb10
panuvit.chantara <panuvit.chantara@ait.asia>
2020-10-24 09:23:50 +0700

    Panuvit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_10_18_152039) do
     create_table "quotations",force: :cascade do |t|
         t.string "author_name"
         t.text "category"
